a9886e39a0fcd581e20ea7b38bba7f65d299a96e
ericstober <erictstober@gmail.com>
2025-01-02 17:10:34 -0600

    added responsive menu for small/mobile screens

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,6 +1,7 @@
 import ModeToggle from "@/components/shared/header/mode-toggle";
 import { Button } from "@/components/ui/button";
-import { ShoppingCart, UserIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
 import Link from "next/link";
 
 const Menu = () => {
@@ -19,6 +20,33 @@ const Menu = () => {
             <UserIcon /> Sign In
           </Link>
         </Button>
+      </nav>
+
+      <nav className='md:hidden'>
+        <Sheet>
+          <SheetTrigger className='align-middle'>
+            <EllipsisVertical />
+          </SheetTrigger>
+
+          <SheetContent className='flex flex-col items-start'>
+            <SheetTitle>Menu</SheetTitle>
+            <ModeToggle />
+
+            <Button asChild variant='ghost'>
+              <Link href='/cart'>
+                <ShoppingCart /> Cart
+              </Link>
+            </Button>
+
+            <Button asChild>
+              <Link href='/sign-in'>
+                <UserIcon /> Sign In
+              </Link>
+            </Button>
+
+            <SheetDescription></SheetDescription>
+          </SheetContent>
+        </Sheet>
       </nav>
     </div>
   );
