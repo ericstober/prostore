85841b7bb96bc3f7111312a1c155253ac7f0e112
ericstober <erictstober@gmail.com>
2025-01-16 22:34:59 -0600

    add User, Account, Session & VerificationToken models to schema.prisma and fixed prisma migration issue by resetting the migrations and reseeding the database

--- prisma/migrations/20250108031451_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isFeatures` on the `Product` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Product" DROP COLUMN "isFeatures",
-ADD COLUMN     "isFeatured" BOOLEAN NOT NULL DEFAULT false;
