5b541f3e16aac4f98ba3d5e4e3711001bfdedbc1
ericstober <erictstober@gmail.com>
2025-01-25 14:10:46 -0600

    add middleware.ts and add session cart id cookie

--- auth.ts
+++ auth.ts
@@ -4,6 +4,8 @@ import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export const config = {
   pages: {
@@ -71,19 +73,43 @@ export const config = {
         token.role = user.role;
 
         // If user has no name then use email
-        if (user.name ==='NO_NAME') {
-          token.name = user.email!.split('@')[0];
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
 
           // Update the database to reflect token name
           await prisma.user.update({
-            where: {id: user.id},
-            data: {name: token.name}
-          })
+            where: { id: user.id },
+            data: { name: token.name },
+          });
         }
       }
 
       return token;
-    }
+    },
+    authorized({ request, auth }: any) {
+      // Check for session cart cookie
+      if (!request.cookies.get("sessionCartId")) {
+        // Generate new session cart id cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone the request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        // Set newly generated sessionCartId in the response cookies
+        response.cookies.set("sessionCartId", sessionCartId);
+
+        return response;
+      } else {
+        return true;
+      }
+    },
   },
 } satisfies NextAuthConfig;
 
