5d2c2fe9f06529fb765c7603cef99ed0f31c4399
ericstober <erictstober@gmail.com>
2025-04-06 17:58:57 -0500

    add rechart to admin overview page

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -6,6 +6,7 @@ import { formatCurrency, formatDateTime, formatNumber } from "@/lib/utils";
 import { BadgeDollarSign, Barcode, CreditCard, Users } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
+import Charts from "./charts";
 
 export const metadata: Metadata = {
   title: "Admin Overview",
@@ -77,7 +78,13 @@ const AdminOverviewPage = async () => {
             <CardTitle>Overview</CardTitle>
           </CardHeader>
 
-          <CardContent>{/* Chart here */}</CardContent>
+          <CardContent>
+            <Charts
+              data={{
+                salesData: summary.salesData,
+              }}
+            />
+          </CardContent>
         </Card>
 
         <Card className='col-span-3'>
