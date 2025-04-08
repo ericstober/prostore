2f39bc196e0bf0b6871d167e0c1f7df73b0f7bd1
ericstober <erictstober@gmail.com>
2025-04-07 19:27:10 -0500

    add productDefaultValues

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -24,6 +24,21 @@ export const shippingAddressDefaultValues = {
   country: "",
 };
 
+export const productDefaultValues = {
+  name: "",
+  slug: "",
+  category: "",
+  images: [],
+  brand: "",
+  description: "",
+  price: "0",
+  stock: "0",
+  rating: "0",
+  numReviews: "0",
+  isFeatured: false,
+  banner: null,
+};
+
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
   ? process.env.PAYMENT_METHODS.split(", ")
   : ["PayPal", "Stripe", "CashOnDelivery"];
