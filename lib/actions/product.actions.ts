1d165cc241fdfc0602fde1e44b38b23bcb2e2fc5
ericstober <erictstober@gmail.com>
2025-04-07 22:14:50 -0500

    comment out getAllProducts query and category params since currently unused

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -25,11 +25,11 @@ export async function getProductBySlug(slug: string) {
 
 // Get all products
 export async function getAllProducts({
-  query,
+  // query,
   limit = PAGE_SIZE,
   page,
-  category,
-}: {
+}: // category,
+{
   query: string;
   limit?: number;
   page: number;
