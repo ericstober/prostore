15012096fe9cffccaa5f5aede12bb32d36e4e389
ericstober <erictstober@gmail.com>
2025-04-09 10:31:56 -0500

    fix edit link

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -59,7 +59,7 @@ const AdminProductsPage = async (props: {
               <TableCell>{product.rating}</TableCell>
               <TableCell className='flex gap-1'>
                 <Button asChild variant='outline' size='sm'>
-                  <Link href={`/admin/product/${product.id}`}>Edit</Link>
+                  <Link href={`/admin/products/${product.id}`}>Edit</Link>
                 </Button>
 
                 <DeleteDialog id={product.id} action={deleteProduct} />
