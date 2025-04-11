bd7402517fd2373ec832a673b6999a11e46893bd
ericstober <erictstober@gmail.com>
2025-04-10 22:01:09 -0500

    add search to admin users page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -16,6 +16,7 @@ import { ShippingAddress } from "@/types";
 import { z } from "zod";
 import { PAGE_SIZE } from "../constants";
 import { revalidatePath } from "next/cache";
+import { Prisma } from "@prisma/client";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
@@ -191,8 +192,19 @@ export async function updateProfile(user: { name: string; email: string }) {
 }
 
 // Get all the users
-export async function getAllUsers({ limit = PAGE_SIZE, page }: { limit?: number; page: number }) {
+export async function getAllUsers({ limit = PAGE_SIZE, page, query }: { limit?: number; page: number; query: string }) {
+  const queryFilter: Prisma.UserWhereInput =
+    query && query !== "all"
+      ? {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          } as Prisma.StringFilter,
+        }
+      : {};
+
   const data = await prisma.user.findMany({
+    where: { ...queryFilter },
     orderBy: { createdAt: "desc" },
     take: limit,
     skip: (page - 1) * limit,
