d9654b28f216331efe9bc3437909eb8020f6ace2
ericstober <erictstober@gmail.com>
2025-04-12 22:33:06 -0500

    remove accidental title import

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,5 +1,3 @@
-import { title } from "process";
-
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Prostore";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern e-commerce platform built with Next.js";
