67084024bd9c1d8da3bfe15fdc6cd3b52ad43e04
ericstober <erictstober@gmail.com>
2025-04-12 22:32:15 -0500

    add review form default values

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,5 @@
+import { title } from "process";
+
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Prostore";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern e-commerce platform built with Next.js";
@@ -37,6 +39,12 @@ export const productDefaultValues = {
   numReviews: "0",
   isFeatured: false,
   banner: null,
+};
+
+export const reviewFormDefaultVales = {
+  title: "",
+  comment: "",
+  rating: 0,
 };
 
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
