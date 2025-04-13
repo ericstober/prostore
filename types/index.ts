6a69fc3aea128defa644dcd83c1d0af74e1252a3
ericstober <erictstober@gmail.com>
2025-04-12 22:30:32 -0500

    add Review type

--- types/index.ts
+++ types/index.ts
@@ -7,6 +7,7 @@ import {
   insertOrderSchema,
   insertOrderItemSchema,
   paymentResultSchema,
+  insertReviewSchema,
 } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -16,9 +17,13 @@ export type Product = z.infer<typeof insertProductSchema> & {
 };
 
 export type Cart = z.infer<typeof insertCartSchema>;
+
 export type CartItem = z.infer<typeof cartItemSchema>;
+
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+
 export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+
 export type Order = z.infer<typeof insertOrderSchema> & {
   id: string;
   createdAt: Date;
@@ -29,4 +34,11 @@ export type Order = z.infer<typeof insertOrderSchema> & {
   orderitems: OrderItem[];
   user: { name: string; email: string };
 };
+
 export type PaymentResult = z.infer<typeof paymentResultSchema>;
+
+export type Review = z.infer<typeof insertReviewSchema> & {
+  id: string;
+  createdAt: Date;
+  user?: { name: string };
+};
