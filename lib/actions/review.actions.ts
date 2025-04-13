047a82c6b30f91549642c445994a11d2eea1abe3
ericstober <erictstober@gmail.com>
2025-04-13 00:10:10 -0500

    add create and update review action

---
+++ lib/actions/review.actions.ts
@@ -0,0 +1,87 @@
+"use server";
+
+import { z } from "zod";
+import { insertReviewSchema } from "../validators";
+import { formatError } from "../utils";
+import { auth } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { revalidatePath } from "next/cache";
+
+// Create & Update Reviews
+export async function createUpdateReview(data: z.infer<typeof insertReviewSchema>) {
+  try {
+    const session = await auth();
+
+    if (!session) throw new Error("Use is not authenticated");
+
+    // Validate & store the view
+    const review = insertReviewSchema.parse({
+      ...data,
+      userId: session?.user?.id,
+    });
+
+    // Get product that is being reviewed
+    const product = await prisma.product.findFirst({
+      where: { id: review.productId },
+    });
+
+    if (!product) throw new Error("Product not found");
+
+    // Check if user already reviewed product
+    const reviewExists = await prisma.review.findFirst({
+      where: {
+        productId: review.productId,
+        userId: review.userId,
+      },
+    });
+
+    await prisma.$transaction(async (tx) => {
+      if (reviewExists) {
+        // Update review for product
+        await tx.review.update({
+          where: { id: reviewExists.id },
+          data: {
+            title: review.title,
+            description: review.description,
+            rating: review.rating,
+          },
+        });
+      } else {
+        // Create review for product
+        await tx.review.create({ data: review });
+      }
+
+      // Get average rating for the product
+      const averageRating = await tx.review.aggregate({
+        _avg: { rating: true },
+        where: { productId: review.productId },
+      });
+
+      // Get the number of reviews for the product
+      const numReviews = await tx.review.count({
+        where: { productId: review.productId },
+      });
+
+      // Update the rating and number of reviews in product table
+      await tx.product.update({
+        where: { id: review.productId },
+        data: {
+          rating: averageRating._avg.rating || 0,
+          numReviews,
+        },
+      });
+    });
+
+    revalidatePath(`/product/${product.slug}`);
+
+    return {
+      sucess: true,
+      message: "Review updated successfully",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
