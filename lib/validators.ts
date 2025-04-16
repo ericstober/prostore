908728235b1537d016585db77cb203649638cc74
ericstober <erictstober@gmail.com>
2025-04-15 22:19:43 -0500

    add comment

--- lib/validators.ts
+++ lib/validators.ts
@@ -111,6 +111,7 @@ export const insertOrderItemSchema = z.object({
   qty: z.number(),
 });
 
+// Schema for the PayPal paymentResult
 export const paymentResultSchema = z.object({
   id: z.string(),
   status: z.string(),
